dbb87b1c850305a6577abfa64a400d2e13d9601c
codysilverglat <c.silverglat@gmail.com>
2018-11-25 21:45:49 -0600

    Added components

---
+++ public/src/app/property2/property2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property2',
+  templateUrl: './property2.component.html',
+  styleUrls: ['./property2.component.css']
+})
+export class Property2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
