b6a57ea619c861defb9d04443317b2093c558d43
codysilverglat <c.silverglat@gmail.com>
2018-11-26 22:00:42 -0600

    updated main component

--- public/src/app/property1/property1.component.ts
+++ public/src/app/property1/property1.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MainComponent } from '../main/main.component';
 
 @Component({
+  providers: [MainComponent],
   selector: 'app-property1',
   templateUrl: './property1.component.html',
   styleUrls: ['./property1.component.css']
 })
 export class Property1Component implements OnInit {
+  @Input() MainComponent: MainComponent;
 
-  constructor() { }
+  constructor(
+   private _parent: MainComponent 
+  ) { }
 
   ngOnInit() {
   }
+
+
 
 }
