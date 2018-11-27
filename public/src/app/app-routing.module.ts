8f1a48fc3cd6fe9f96f19984eae471beb30c6c56
codysilverglat <c.silverglat@gmail.com>
2018-11-26 21:24:34 -0600

    added routes

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -4,11 +4,18 @@ import { Property1Component } from './property1/property1.component';
 import { Property2Component } from './property2/property2.component';
 import { Property3Component } from './property3/property3.component';
 import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
 import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  {}
+  { path: '', component: MainComponent, children: [
+    { path: 'property1', component: Property1Component },
+    { path: 'property2', component: Property2Component },
+    { path: 'property3', component: Property3Component },
+    { path: 'search', component: SearchComponent }]
+  },
+  {path: '**', redirectTo: '' }
 ];
 
 @NgModule({
