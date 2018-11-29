36556e18cc94e04d114dc603b7aa49095f28f85f
codysilverglat <c.silverglat@gmail.com>
2018-11-28 22:26:30 -0600

    updated '.gitignore' and 'caller.service.ts'

--- public/src/app/caller.service.ts
+++ public/src/app/caller.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CallerService {
 
+  constructor(
+    private http: HttpClient, 
+    ) {
+}
+
+  formAddress1: String = '';
+  formCityStateZip1: String = '';
+
   printToConsole(arg) {
     console.log(arg);
   }
  
+  
 }
