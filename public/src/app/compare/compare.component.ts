5b62bdc9a7eb2789bb29189979075e9fb83810ef
codysilverglat <c.silverglat@gmail.com>
2018-11-30 00:01:43 -0600

    updated tables in compare component

--- public/src/app/compare/compare.component.ts
+++ public/src/app/compare/compare.component.ts
@@ -13,6 +13,10 @@ export class CompareComponent implements OnInit {
     rent1: number = this.rent1;
     rent2: number = this.rent2;
     rent3: number = this.rent3;
+
+    hypotheticalPrice1: number = this.hypotheticalPrice1;
+    hypotheticalPrice2: number = this.hypotheticalPrice2;
+    hypotheticalPrice3: number = this.hypotheticalPrice3;
     
     constructor() { }
     
